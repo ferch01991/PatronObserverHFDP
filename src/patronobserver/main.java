ef85161da0dd10434b6d43222a27445c7908487c
ferch01991 <fercho_youknow@hotmail.com>
2015-12-01 11:34:17 -0500

    Patrón Observer

---
+++ src/patronobserver/main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronobserver;
+import Clases.*;
+import Interfaces.*;
+
+/**
+ *
+ * @author fernando
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WeatherData weatherData = new WeatherData();
+        
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(60, 68, 29.4f);
+        weatherData.setMeasurements(75, 69, 31.4f);
+    }
+    
+}
